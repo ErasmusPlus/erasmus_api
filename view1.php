72fa1a073f7b32e850c611d1ebfc638760bc46cf
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-15 11:20:07 +0300

    Fix query

--- view1.php
+++ view1.php
@@ -17,7 +17,7 @@
        die( print_r( sqlsrv_errors(), true));
   }
 
-  $sql = "SELECT * FROM erasmus_final WHERE 'aem' == ".$aem;
+  $sql = "SELECT * FROM erasmus_final WHERE 'aem' = ".$aem;
 
   $stmt = sqlsrv_query( $conn, $sql, array());
   if( $stmt === false ) {
