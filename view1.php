885bb3f1387dc6bb5be1f661d0a6d25c1f4e13bd
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-09-15 11:10:22 +0300

    Configure and implement view1

---
+++ view1.php
@@ -0,0 +1,32 @@
+<?php
+  require "config.php";
+
+  $conn_details = array(
+    "Database" => $api['db'],
+    "UID" => $api['UID'],
+    "PWD" => $api['PWD']
+  );
+
+  $conn = sqlsrv_connect( $api['host'], $conn_details);
+
+  if( $conn === false ) {
+       die( print_r( sqlsrv_errors(), true));
+  }
+
+  $sql = "SELECT * FROM erasmus_final";
+
+  $stmt = sqlsrv_query( $conn, $sql, array());
+  if( $stmt === false ) {
+       die( print_r( sqlsrv_errors(), true));
+  }
+
+
+  while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+          var_dump($row);
+          echo "<br>";
+  }
+
+  sqlsrv_free_stmt( $stmt);
+
+
+ ?>
