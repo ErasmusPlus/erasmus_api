0826654422850bff7b0027f8a2263f398d042e90
Dimitris Tsiktsiris <tsiktsiris@sch.gr>
2017-10-27 17:35:05 +0300

    Update table

--- view2.php
+++ view2.php
@@ -5,7 +5,7 @@
 
   $aem = $_GET['aem'];
   $depID = $_GET['depID'];
-  
+
   $conn_details = array(
     "Database" => $api['db'],
     "UID" => $api['UID'],
@@ -18,7 +18,7 @@
        die( print_r( sqlsrv_errors(), true));
   }
 
-  $sql = "SELECT * FROM erasmus_final WHERE spec_aem = '$aem' AND depID = '$depID'";
+  $sql = "SELECT * FROM erasmus_final_grade WHERE spec_aem = '$aem' AND depID = '$depID'";
 
   $stmt = sqlsrv_query( $conn, $sql, array());
   if( $stmt === false ) {
